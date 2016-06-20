14bd95e62ddb1a83ec8c20f77159de6cbd9d5502
c45 <test@gmail.com>
2016-06-20 15:13:46 +0700

    ini udah ada contoh form nya

---
+++ addfactor.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Perakiraan Cuaca</title>
+	<link href="Content/css/bootstrap.css" rel="stylesheet">
+	<link href="Content/css/alertify.min.css" rel="stylesheet">
+	<link href="Content/css/default.min.css" rel="stylesheet">
+	<link href="Content/css/bootstrapValidator.min.css" rel="stylesheet">
+    <!-- Font awesome -->
+    <link href="Content/css/font-awesome.min.css" rel="stylesheet">
+	
+	<!--script-->
+	<script src="Scripts/js/jquery.min.js"></script>
+	<script src="Scripts/js/bootstrap.min.js"></script>
+	<script src="Scripts/js/bootstrapValidator.min.js"></script>
+	<script src="Scripts/js/alertify.min.js"></script>
+
+</head>
+<body>
+<br/>
+<div class="row">
+	<div class="col-sm-12 col-md-12 col-xs-12">
+		<div class="col-sm-offset-4 col-md-4 col-xs-4">
+			 <div class="panel panel-success">
+					<div class="panel-heading"> <h3 class="panel-title"></h3> </div>
+				<div class="panel-body">
+			
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+	
+</body>
+
+</html>
